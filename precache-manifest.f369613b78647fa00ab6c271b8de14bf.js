e9ad7c37595f474f04b5cc684e80274a70bf6973
vharvey <vharvey302@gmail.com>
2019-05-22 15:34:56 -0400

    Updates

---
+++ precache-manifest.f369613b78647fa00ab6c271b8de14bf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "76d869bf6eb40f92777ee999dc9fd092",
+    "url": "/email-react/index.html"
+  },
+  {
+    "revision": "48438698fa01c66a2bc2",
+    "url": "/email-react/static/css/main.aa8339bc.chunk.css"
+  },
+  {
+    "revision": "94d358ac89673d784931",
+    "url": "/email-react/static/js/2.7d29f1d3.chunk.js"
+  },
+  {
+    "revision": "48438698fa01c66a2bc2",
+    "url": "/email-react/static/js/main.105d87f5.chunk.js"
+  },
+  {
+    "revision": "8519cbf31d9a4a4baf16",
+    "url": "/email-react/static/js/runtime~main.28143474.js"
+  }
+]);
